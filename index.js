7f67b8cbfda6ff951afc9c246cad87c9ded23abf
DanhCypher <brucelee6788@gmail.com>
2025-08-23 11:12:55 +0700

    update database

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ dotenv.config();
 const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
